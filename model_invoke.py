c39ccd4f54d3aa374db5b87157ab68121f9a4974
totoleon <hailong.leon@gmail.com>
2023-10-23 15:12:48 -0700

    Update model_invoke.py

--- model_invoke.py
+++ model_invoke.py
@@ -1,31 +1,11 @@
 import torch
-from diffusers import StableDiffusionXLPipeline, StableDiffusionXLImg2ImgPipeline
+from diffusers import AutoPipelineForText2Image
 
-# load both base & refiner
-base = StableDiffusionXLPipeline.from_pretrained(
+pipeline = AutoPipelineForText2Image.from_pretrained(
     "stabilityai/stable-diffusion-xl-base-1.0", torch_dtype=torch.float16, variant="fp16", use_safetensors=True
 ).to("cuda")
 
-refiner = StableDiffusionXLImg2ImgPipeline.from_single_file(
-    "stabilityai/stable-diffusion-xl-refiner-1.0", torch_dtype=torch.float16, use_safetensors=True, variant="fp16"
-).to("cuda")
-
-# Define how many steps and what % of steps to be run on each experts (80/20) here
-n_steps = 40
-high_noise_frac = 0.8
-
 def invoke(input_text):
-    image = base(
-        prompt=input_text,
-        num_inference_steps=n_steps,
-        denoising_end=high_noise_frac,
-        output_type="latent",
-    ).images
-    image = refiner(
-        prompt=input_text,
-        num_inference_steps=n_steps,
-        denoising_start=high_noise_frac,
-        image=image,
-    ).images[0]
+    image = pipeline(prompt=input_text).images[0]
     image.save('generated_image.png')
     return "generated_image.png"
