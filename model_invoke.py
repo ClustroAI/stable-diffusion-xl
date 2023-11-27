71843d6b876daa497d5c50b89d70ff299371aa31
Yingqiang Liu <53621227+Remilistrasza@users.noreply.github.com>
2023-11-27 12:55:24 -0700

    Specify model version for pipeline

--- model_invoke.py
+++ model_invoke.py
@@ -2,7 +2,11 @@ import torch
 from diffusers import AutoPipelineForText2Image
 
 pipeline = AutoPipelineForText2Image.from_pretrained(
-    "stabilityai/stable-diffusion-xl-base-1.0", torch_dtype=torch.float16, variant="fp16", use_safetensors=True
+    "stabilityai/stable-diffusion-xl-base-1.0", 
+    torch_dtype=torch.float16, 
+    variant="fp16", 
+    use_safetensors=True,
+    revision=("462165984030d82259a11f4367a4eed129e94a7b")
 ).to("cuda")
 
 def invoke(input_text):
